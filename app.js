80cd996f8c07e6686fdb4eff89eeac461f82689b
Reese Dick <reeserdick@gmail.com>
2019-12-23 12:00:50 -0500

    pushing reese branch to github, created server models and db and app

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 var express = require('express');
 var app = express();
 
-app.listen(3000, function() {
+app.listen(process.env.PORT, function() {
     console.log('App is listening on 3000!')
 })
